f0b7e54cf8638c96df251771f15a9a47ba373759
tycrek <josh.moore@jmoore.dev>
2020-09-02 09:54:22 -0600

    Improved print statements and overhauled code

--- 2q2t.go
+++ 2q2t.go
@@ -9,50 +9,138 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
-func main() {
-	fmt.Println("\nWelcome to 2queue2timer")
-	fmt.Println("\nAfter pressing Enter, move your mouse pointer to the Play button in the server list for 2b2t.")
-	fmt.Println("After 3 seconds, 2q2t will capture that position.")
-	fmt.Println("Press Enter to continue...")
+/* Strings */
+var pressEnter = "\nPress Enter to continue..."
+var done = "\n\nLiftoff! Joining 2b2t queue\n"
+var starting = "Starting countdown (you may not see any output for up to 60 seconds)\n"
 
+var step_1 = "\nStep 1: Capture mouse pointer"
+var step_1_1 ="  - Once you press Enter, you have 3 seconds to position your mouse pointer over the play button for 2b2t"
+var step_1_2 ="  - Have Minecraft in windowed mode to prevent any problems"
+var step_1_3 ="  - To cancel at any point in time, press CTRL+C"
+
+
+var step_2 = "\nStep 2: Enter queue time"
+var step_2_1 = "  - Enter a time for 2q2t to join the queue"
+var step_2_2 = "  - At this time, 2q2t will auto click the play button to join the queue"
+var step_2_3 = "  - Use 24-hour time (example: 10 PM = 22)"
+var step_2_4 = "  - Use the following format: HH:MM"
+
+var step_3 = "\nStep 3: Wait"
+var step_3_1 = "  - When this countdown reaches 0, 2q2t will click the play button"
+var step_3_2 = "  - Make sure the play button is visible"
+var step_3_3 = "  - Do not move the Minecraft window from where it is now until you have joined queue"
+var step_3_4 = "  - Disable automatic sleep or lock on your PC"
+
+/* Variables */
+var x int
+var y int
+var userTime string
+
+/* Functions */
+
+// Prints the remaining time until click
+func printRemaining(remaining int) {
+	if remaining >= 60 && remaining % 60 == 0 {
+		fmt.Print("\r", remaining / 60, " minutes left                ")
+	} else if remaining < 60 && remaining > 10 && remaining % 10 == 0 {
+		fmt.Print("\r", remaining, " seconds left                ")
+	} else if remaining < 10 {
+		fmt.Print("\r", remaining, " seconds left                ")
+	}
+}
+
+// Clicks the mouse
+func click() {
+	fmt.Println(done)
+	robotgo.MoveMouse(x, y)
+	robotgo.Click()
+}
+
+// Print welcome message
+func welcome() {
+	fmt.Println("\n")
+	fmt.Println("######################################")
+	fmt.Println("##                                  ##")
+	fmt.Println("##         ~ 2queue2timer ~         ##")
+	fmt.Println("##                                  ##")
+	fmt.Println("##         Author:  tycrek          ##")
+	fmt.Println("##        Website:  jmoore.dev      ##")
+	fmt.Println("##                                  ##")
+	fmt.Println("######################################")
+	fmt.Println("\n")
+}
+
+// Step 1: record mouse position
+func step1() {
+	fmt.Println(step_1)
+	fmt.Println(step_1_1)
+	fmt.Println(step_1_2)
+	fmt.Println(step_1_3)
+	fmt.Println(pressEnter)
 	fmt.Scanln()
 
+	// Wait 3 seconds and record mouse position
 	time.Sleep(3 * time.Second)
-	
-	x, y := robotgo.GetMousePos()
-	fmt.Println("Mouse recorded at: ", x, y)
+	x, y = robotgo.GetMousePos()
+	fmt.Println("Mouse recorded at: [", x, "] [", y, "]")
+}
 
-	fmt.Println("\nNext enter a time to queue. Follow this format:")
-	fmt.Println("    HH:MM")
-	fmt.Println("Make sure to use 24 hour time (i.e. 10 PM = 22)")
+// Step 2: Get time to queue from user
+func step2() {
+	fmt.Println(step_2)
+	fmt.Println(step_2_1)
+	fmt.Println(step_2_2)
+	fmt.Println(step_2_3)
+	fmt.Println(step_2_4)
 
-	var qts string
 	fmt.Print("> ")
-	fmt.Scanln(&qts)
+	fmt.Scanln(&userTime)
+	fmt.Println("Will queue at: ", userTime)
+}
 
-	fmt.Println("Will queue at:", qts, "Make sure you have sleep and lock DISABLED on your PC. Also do not move the Minecraft window until the program has joined queue.")
+// Step 3: Count down and click play
+func step3() {
+	fmt.Println(step_3)
+	fmt.Println(step_3_1)
+	fmt.Println(step_3_2)
+	fmt.Println(step_3_3)
+	fmt.Println(step_3_4)
+	fmt.Println(pressEnter)
+	fmt.Scanln()
+	fmt.Println(starting)
 
-	qtsplit := strings.Split(qts, ":")
+	// Split the user entered string to get the hour and minute components
+	qtsplit := strings.Split(userTime, ":")
 	qthour, _ := strconv.Atoi(qtsplit[0])
 	qtminute, _ := strconv.Atoi(qtsplit[1])
 
+	// Get the current time and the users timezone
 	t := time.Now()
 	tzone, offset := t.Zone()
 	loc :=  time.FixedZone(tzone, offset)
 
+	// Creat a Date object for the queue time
 	qt := time.Date(t.Year(), t.Month(), t.Day(), qthour, qtminute, 0, 0, loc)
-	
+
+	// Loop until it is time to queue
 	for {
-		if int(time.Until(qt).Seconds()) > 1 {
-			fmt.Println(int(time.Until(qt).Seconds()), " seconds left")
+		remaining := int(time.Until(qt).Seconds())
+		if remaining > 0 {
+			printRemaining(remaining)
 			time.Sleep(1 * time.Second)
 		} else {
 			break
 		}
 	}
-	fmt.Println("yeet")
 
-	robotgo.MoveMouse(x, y)
-	robotgo.Click()
+	click()
+}
 
+/* Main */
+func main() {
+	welcome()
+	step1()
+	step2()
+	step3()
 }
